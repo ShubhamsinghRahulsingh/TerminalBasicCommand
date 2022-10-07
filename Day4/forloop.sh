cdfdbc140d213b123dbfccea39ba67abc84edb8d
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-07 12:20:17 +0530

    Modified Day4 workhop program

--- Day4/forloop.sh
+++ Day4/forloop.sh
@@ -3,11 +3,13 @@ do
 	folderName=`echo $files | awk -F. '{print $1}'`;
 	echo $files;
 	echo $folderName;
-	printf "\n";
-	if [ -d $folderName ]
-	then
-		rm -r $folderName;
-	fi
+
 	mkdir $folderName;
 	cp $files $folderName/$files;
-done
+
+        echo copied $files to $folderName/;
+        now=$(date +"%m_%d_%y")
+        echo $now;
+        nfile=$folderName-$now.log
+        mv $folderName/$files $folderName/$nfile
+done 
