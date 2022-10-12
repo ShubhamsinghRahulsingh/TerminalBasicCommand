ea314f171cc3a2c7d3861a0309861b33ce19e53c
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-12 09:36:03 +0530

    Added: Day5 practice problems

--- Day5/practice2/unitconv.sh
+++ Day5/practice2/unitconv.sh
@@ -2,16 +2,16 @@
 
 read -p "Enter the value which you want to convert: " value;
 
-inch=$( echo $value 12 | awk 'print{$1*$2}')
+inch=$(($value*12))
 echo "$value feet into inches is $inch"
 
-meter=`echo $(($value * .3048)) | awk -F. '{print $1}'`;
+meter=`awk 'BEGIN{printf("%0.2f",'$value'*.3048)}'`
 echo "$value feet into meter is $meter"
 
-feet=`echo $(($value * .0833)) | awk -F. '{print $1}'`;
+feet=`awk 'BEGIN{printf("%0.2f",'$value'*(1/12))}'`
 echo "$value inch into feet is $feet"
 
-feet=`echo $(($value / .30480)) | awk -F. '{print $1}'`;
+feet=`awk 'BEGIN{printf("%0.2f",'$value'/0.3048)}'`
 echo "$value meter into feet is $feet" 
 
 
